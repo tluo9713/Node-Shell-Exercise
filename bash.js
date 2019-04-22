7c8096ddde74539a7653bb3a8e05121653cc58ff
Tristan Watanabe <tristan.watanabe@gmail.com>
2019-04-22 13:04:31 -0400

    added cat

--- bash.js
+++ bash.js
@@ -1,15 +1,20 @@
 const pwd = require('./pwd');
 const ls = require('./ls');
+const cat = require('./cat');
 
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', data => {
+  
   const cmd = data.toString().trim();
   if (cmd === 'pwd') {
       pwd();
   } 
   else if (cmd === 'ls') {
       ls();
+  } else if (cmd.startsWith('cat')) {
+    let filePath = cmd.split(' ')[1];
+    cat(filePath);
   } else {
   process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\nprompt > ');

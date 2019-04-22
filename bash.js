cae52e05cde4666cb738b7b36b6a0f848c45d888
Thomas Luo <tluo9713@gmail.com>
2019-04-22 12:39:28 -0400

    fixing nodemon

--- bash.js
+++ bash.js
@@ -1,8 +1,13 @@
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', data => {
-  const cmd = data; //.toString().trim();
+  const cmd = data.toString().trim();
+  if (cmd === 'pwd') {
+    console.log(process.cwd());
 
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
+    //process.stdout.write(process.cwd);
+  } else {
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+  }
 });
